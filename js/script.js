990cf019faf4156e189812880fee8ae09c1e4d64
Martin Grand <marton.nagy@martingrand.com>
2017-12-07 07:54:52 +0100

    some change

--- js/script.js
+++ js/script.js
@@ -10,4 +10,12 @@
     document.querySelector('[data-open-overlay]').addEventListener('click', function () {
         document.querySelector('[data-wrapper]').setAttribute('data-wrapper', 'overlay');
     });
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function () {
+        dataLayer.push(arguments);
+    };
+
+    gtag('js', new Date());
+    gtag('config', 'UA-38311615-10');
 })();
